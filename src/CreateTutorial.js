359f29fab0e514b48967f8e380f436e09fd5e641
Om <36092598+Khetwal-om@users.noreply.github.com>
2020-11-03 17:02:37 +0530

    update form phase III it's working 😎

--- src/CreateTutorial.js
+++ src/CreateTutorial.js
@@ -3,12 +3,54 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createsingleTutorial } from './store/actions/projectActions'
 import './CreateTutorial.css'
+import FlashcardForm from './FlashcardForm'
 
 class CreateTutorial extends Component {
   state = {
     name: '',
     link: '',
-    flashcards: []
+    flashcards: [
+      {
+        question: '',
+        explanation: '',
+        exampleOne: '',
+        exampleTwo: ''
+      },
+      {
+        question: '',
+        explanation: '',
+        exampleOne: '',
+        exampleTwo: ''
+      }
+    ]
+  }
+  questionUpdate = (e, index) => {
+    const flashcards = this.state.flashcards
+    flashcards[index].question = e.target.value
+    this.setState({
+      flashcards
+    })
+  }
+  explanationUpdate = (e, index) => {
+    const flashcards = this.state.flashcards
+    flashcards[index].explanation = e.target.value
+    this.setState({
+      flashcards
+    })
+  }
+  exampleOneUpdate = (e, index) => {
+    const flashcards = this.state.flashcards
+    flashcards[index].exampleOne = e.target.value
+    this.setState({
+      flashcards
+    })
+  }
+  exampleTwoUpdate = (e, index) => {
+    const flashcards = this.state.flashcards
+    flashcards[index].exampleTwo = e.target.value
+    this.setState({
+      flashcards
+    })
   }
 
   handleChange = (e) => {
@@ -43,6 +85,30 @@ class CreateTutorial extends Component {
               id="link"
               onChange={this.handleChange}
             />
+          </div>
+          <div className="App">
+            {this.state.flashcards.map((phone, i) => {
+              return (
+                <FlashcardForm
+                  questionUpdate={(e) => {
+                    this.questionUpdate(e, i)
+                  }}
+                  question={this.state.flashcards[i].question}
+                  explanationUpdate={(e) => {
+                    this.explanationUpdate(e, i)
+                  }}
+                  explanation={this.state.flashcards[i].explanation}
+                  exampleOneUpdate={(e) => {
+                    this.exampleOneUpdate(e, i)
+                  }}
+                  exampleOne={this.state.flashcards[i].exampleOne}
+                  exampleTwoUpdate={(e) => {
+                    this.exampleTwoUpdate(e, i)
+                  }}
+                  exampleTwo={this.state.flashcards[i].exampleTwo}
+                />
+              )
+            })}
           </div>
           <div className="input-field">
             <button className="btn">Create</button>
---
+++ src/FlashcardForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './FlashcardForm.css'
+class FlashcardForm extends React.Component {
+  render() {
+    return (
+      <div className="Flashcardform">
+        <input
+          type="text"
+          placeholder="question"
+          onChange={this.props.questionUpdate}
+          value={this.props.question}
+        />
+        <input
+          type="text"
+          placeholder="explanation"
+          onChange={this.props.explanationUpdate}
+          value={this.props.explanation}
+        />
+        <input
+          type="text"
+          placeholder="exampleOne"
+          onChange={this.props.exampleOneUpdate}
+          value={this.props.exampleOne}
+        />
+        <input
+          type="text"
+          placeholder="exampleTwo"
+          onChange={this.props.exampleTwoUpdate}
+          value={this.props.exampleTwo}
+        />
+      </div>
+    )
+  }
+}
+
+export default FlashcardForm
